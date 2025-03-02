04eadc71bbaab202b4cf96b8af1254acf4961022
Nathan P. Bombana <nathan.pbombana@gmail.com>
2025-03-02 02:53:51 -0300

    feat: build basic configuration and CLI args loader

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+mod config;
 mod data;
 mod io;
 
 fn main() {
-    println!("Hello, world!");
+    let cfg_result = config::init();
+    if let Err(e) = cfg_result {
+        println!("Unable to load config file: {}", e);
+        return;
+    }
+
+    let (_config, _args, config_file) = cfg_result.unwrap();
+    println!(
+        "Loaded configuration file from {}",
+        config_file.canonicalize().ok().unwrap().display()
+    );
 }
